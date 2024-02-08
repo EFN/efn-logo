50c2a5636777f838b776229a4b6c576b3d4f6b1e
BFG <Tom.Fredrik@blenning.no>
2024-02-08 12:25:39 +0100

    Add some scripts to produce bitmaps.

---
+++ produce/efnlogo-3000x3000.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+outputname=$(basename $0 .sh).png
+
+convert -background transparent -density 3000 -resize 3000x3000 -gravity center -extent 3000x3000  vectorized/efnlogo.svg bitmap/$outputname
