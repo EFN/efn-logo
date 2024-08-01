d021f06c1754bfb8d65c79a6525eb49928cb7f29
BFG <Tom.Fredrik@blenning.no>
2024-08-01 11:28:16 +0200

    Using actual light blue color.

--- produce/unofficial/efnlogo-lightblueback.sh
+++ produce/unofficial/efnlogo-lightblueback.sh
@@ -5,7 +5,7 @@ outputname=$(basename $0 .sh)
 if [ ! -f vectorized/unofficial/$outputname.svg ]
 then
     [ ! -d vectorized/unofficial ] && mkdir vectorized/unofficial
-    cat vectorized/efnlogo.svg | sed -e 's/#f7931d/#8888ff/' > vectorized/unofficial/$outputname.svg
+    cat vectorized/efnlogo.svg | sed -e 's/#f7931d/#ADD8E6/' > vectorized/unofficial/$outputname.svg
 fi
 
 [ ! -d bitmap/unofficial ] && mkdir bitmap/unofficial
